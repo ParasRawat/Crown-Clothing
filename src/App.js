37c05d9b67fd702b47c8b9b6487786d5eaf5ca4b
ParasRawat <parasmyname@gmail.com>
2020-05-21 16:49:29 +0530

    shop page routing, data js, collection preview component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
 
+import ShopPage from '././pages/shop/shop.component'
 
-const HastPage = ()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
+// const HastPage = ()=>(
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// )
 function App() {
   return (
     <div>
     <Switch>
     <Route exact path="/" component={Homepage}/>
-    <Route exact path="/hats" component={HastPage}/>
+    <Route exact path="/shop" component={ShopPage}/>
     </Switch>
     </div>
   );
