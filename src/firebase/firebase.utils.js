ed51d5c41de64b3367f93d52fbedca5c1b753697
ParasRawat <parasmyname@gmail.com>
2020-06-09 20:19:55 +0530

    removing creds

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,14 +4,7 @@ import 'firebase/auth'
 
 const config = {
     
-        apiKey: "AIzaSyDY38cUVTaVrxu99qeLqSxZa1dK_flhHhM",
-        authDomain: "crown-db-b409b.firebaseapp.com",
-        databaseURL: "https://crown-db-b409b.firebaseio.com",
-        projectId: "crown-db-b409b",
-        storageBucket: "crown-db-b409b.appspot.com",
-        messagingSenderId: "711502642888",
-        appId: "1:711502642888:web:2b99cf95948af1f1bfeffd",
-        measurementId: "G-F0EPEJS0SH"
+        
       };
 
     firebase.initializeApp(config);
