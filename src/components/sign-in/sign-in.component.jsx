8040b0ef3a3a1ab40fb78543907c3452cc959850
ParasRawat <parasmyname@gmail.com>
2020-06-09 20:10:51 +0530

    adding firebase, google sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './sign-in.styles.scss'
 import FormInput from './../form-input/form-input.component'
-
+import {signInWithGoogle} from '../../firebase/firebase.utils'
 import CustomButton from './../custom-button/custom-button.component'
 
 class SignIn extends React.Component{
@@ -60,6 +60,7 @@ class SignIn extends React.Component{
                     required/>
                  
                     <CustomButton type="submit" value= "Submit Form"> Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}> Sign In With Google</CustomButton>
                 </form>
             </div>
         )
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    
+        apiKey: "AIzaSyDY38cUVTaVrxu99qeLqSxZa1dK_flhHhM",
+        authDomain: "crown-db-b409b.firebaseapp.com",
+        databaseURL: "https://crown-db-b409b.firebaseio.com",
+        projectId: "crown-db-b409b",
+        storageBucket: "crown-db-b409b.appspot.com",
+        messagingSenderId: "711502642888",
+        appId: "1:711502642888:web:2b99cf95948af1f1bfeffd",
+        measurementId: "G-F0EPEJS0SH"
+      };
+
+    firebase.initializeApp(config);
+
+    export const auth = firebase.auth();
+    export const firestore= firebase.firestore();
+
+    const provider =new firebase.auth.GoogleAuthProvider();
+
+    provider.setCustomParameters({prompt:'select_account'});
+
+    export const signInWithGoogle = () => {
+        
+        return auth.signInWithPopup(provider);
+
+    }
+
+    export default firebase;
