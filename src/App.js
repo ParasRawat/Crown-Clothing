03ac68f96f4daf90e969c0d4515c2de538b4a3f1
ParasRawat <parasmyname@gmail.com>
2020-06-30 15:56:40 +0530

    userAuthenticaton from a centralized  firestore for both google auth and custom signIn

--- src/App.js
+++ src/App.js
@@ -23,9 +23,31 @@ class App extends React.Component {
 
 
   componentDidMount(){
-   this.unsubscribeFromAuth= auth.onAuthStateChanged( async user =>{
-      createUserProfileDocument(user)
-      console.log(user);
+   this.unsubscribeFromAuth= auth.onAuthStateChanged( async userAuth =>{
+
+    if(userAuth){
+
+        const userRef = await createUserProfileDocument(userAuth)
+        
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          } , ()=>{
+            console.log(this.state)
+          })
+        })
+
+       
+    }
+
+    else{
+
+      this.setState({currentUser:userAuth})
+    }
+
     })
   }
 
