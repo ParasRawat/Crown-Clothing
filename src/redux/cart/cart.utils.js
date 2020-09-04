3c8b2ea88d28d2377b3205d2d42f7302c6c85604
ParasRawat <parasmyname@gmail.com>
2020-09-04 15:36:38 +0530

    grouping similar items by the quantity added

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,26 @@
+ //UTILITY FUNCTIONS ALLOW US TO KEEP OUR FILES CLEAN AND ORGANIZE FUNCTIONS THAT WE MAY NEED IN MULTIPLE FILES IN ONE LOCATION 
+ export const addItemToCart = (cartItems, cartItemToAdd) =>{
+     // maybe implementing the map functionality at this point 
+     const existingCartItem = cartItems.find((cartItem)=> cartItem.id===cartItemToAdd.id);
+
+
+     if(existingCartItem){
+
+        return cartItems.map(cartItem => {
+
+            if(cartItem.id===cartItemToAdd.id){
+                return {
+                    ...cartItem, quantity: cartItem.quantity+1
+                }
+            }
+            else return cartItem
+
+            
+        } )
+     }
+     // return a new array with an additional object 
+
+     else {
+         return [...cartItems, {...cartItemToAdd, quantity:1 }]
+     }
+ }
